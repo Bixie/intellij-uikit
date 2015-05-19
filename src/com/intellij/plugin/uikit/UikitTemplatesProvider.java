cc52568bf198b3aac067fed94142eeb54407af7e
malles <matthijs@bixie.nl>
2015-05-19 15:02:41 +0200

    init repo

---
+++ src/com/intellij/plugin/uikit/UikitTemplatesProvider.java
@@ -0,0 +1,23 @@
+package com.intellij.plugin.uikit;
+
+/**
+* @author Matthijs Alles
+*/
+
+import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider;
+import org.jetbrains.annotations.Nullable;
+
+public class UikitTemplatesProvider implements DefaultLiveTemplatesProvider {
+    @Override
+    public String[] getDefaultLiveTemplateFiles() {
+        return new String[]{
+            "liveTemplates/Uikit",
+        };
+    }
+
+    @Nullable
+    @Override
+    public String[] getHiddenLiveTemplateFiles() {
+       return null;
+    }
+}
